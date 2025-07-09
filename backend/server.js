0f3ac4bea1df517e20ae556a28f493cd8f6782a7
AhsanHafeez-dev <ahsanhafeez724@gmail.com>
2025-07-09 22:04:30 +0500

    winston logger added

--- backend/server.js
+++ backend/server.js
@@ -19,6 +19,31 @@ app.use(
   })
 );
 
+import { logger } from "./utils/logger.js";
+import morgan from "morgan";
+
+const morganFormat = ":method :url :status :response-time ms";
+
+app.use(
+  morgan(morganFormat, {
+    stream: {
+      write: (message) => {
+        const logObject = {
+          method: message.split(" ")[0],
+          url: message.split(" ")[1],
+          status: message.split(" ")[2],
+          responseTime: message.split(" ")[3],
+        };
+        logger.info(JSON.stringify(logObject));
+      },
+    },
+  })
+);
+
+
+
+
+
 
 import { authRoutes } from "./routes/auth-routes/index.js"
 import {instructorCourseRoutes} from "./routes/instructor-routes/course-routes.js"
@@ -52,4 +77,4 @@ app.use((error, req, res, next) => {
 
 
 const port = process.env.PORT || 5000;
-app.listen(port, () => { console.log(`app listening on port ${port}`) });
+app.listen(port, () => { console.log(`app listening on port ${port}`); logger.info(`app listening on port ${port}`);});

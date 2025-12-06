251179ed0e21ab0535061d228c565ab1f536c5dd
AhsanHafeez-dev <ahsanhafeez724@gmail.com>
2025-12-06 21:11:25 +0500

    initialized

--- routes/instructor-routes/media-routes.js
+++ routes/instructor-routes/media-routes.js
@@ -11,7 +11,8 @@ import { authenticate } from "../../middleware/auth-middleware.js";
 
 const mediaRoutes = express.Router();
 
-const upload = multer({ dest: "uploads/"});
+// const upload = multer({ dest: "uploads/" });
+const upload = multer();
 // import {upload} from "../../middleware/multer.middleware.js"
 mediaRoutes.use(authenticate)
 

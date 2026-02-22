e86e8135c9016cda905606a860785f54041c9324
AhsanHafeez-dev <ahsanhafeez724@gmail.com>
2026-02-23 02:32:11 +0500

    optimizations

--- middleware/multer.middleware.js
+++ middleware/multer.middleware.js
@@ -2,17 +2,18 @@ import multer from "multer";
 import path from "path";
 
 
-const storage = multer.diskStorage({
-  destination: function (req, file, cb) {
+// const storage = multer.diskStorage({
+//   destination: function (req, file, cb) {
     
-    cb(null, "/upload");
-  },
-  filename: function (req, file, cb) {
+//     cb(null, "/upload");
+//   },
+//   filename: function (req, file, cb) {
     
-    cb(null, file.originalname);
-  },
-});
+//     cb(null, file.originalname);
+//   },
+// });
 // const storage=multer.memoryStorage()
+const storage = multer.memoryStorage();
 export const upload = multer({
   storage,
 });

f159ac17bc5e25c819e7b8bdcc767c1d90d94652
AhsanHafeez-dev <ahsanhafeez724@gmail.com>
2026-02-23 02:19:03 +0500

    optimizations

--- server.js
+++ server.js
@@ -39,22 +39,22 @@ import morgan from "morgan";
 
 const morganFormat = ":method :url :status :response-time ms";
 
-app.use(
-  morgan(morganFormat, {
-    stream: {
-      write: (message) => {
-        const logObject = {
-          method: message.split(" ")[0],
-          url: message.split(" ")[1],
-          status: message.split(" ")[2],
-          responseTime: message.split(" ")[3],
+// app.use(
+//   morgan(morganFormat, {
+//     stream: {
+//       write: (message) => {
+//         const logObject = {
+//           method: message.split(" ")[0],
+//           url: message.split(" ")[1],
+//           status: message.split(" ")[2],
+//           responseTime: message.split(" ")[3],
           
-        };
-        logger.info(JSON.stringify(logObject));
-      },
-    },
-  })
-);
+//         };
+//         logger.info(JSON.stringify(logObject));
+//       },
+//     },
+//   })
+// );
 
 
 
